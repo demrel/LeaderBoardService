d4ec1ccb4a7991f30b246ae75afcb90d5d2e1d1b
demrel qara <qarademrel@gmail.com>
2020-12-19 13:02:36 +0400

    remove init

--- Helper/ApplicationDbInitializer.cs
+++ Helper/ApplicationDbInitializer.cs
@@ -9,25 +9,7 @@ namespace LeaderBoardService.Helper
         public static void SeedUsers(UserManager<AppUser> userManager,RoleManager<IdentityRole> roleManager)
         {
          
-
-            if (userManager.FindByNameAsync("admin").Result == null)
-            {
-                AppUser user = new AppUser
-                {
-                    UserName = "admin",
-                };
-               
-                IdentityResult result = userManager.CreateAsync(user, "Admin1234").Result;
-
-                if (result.Succeeded)
-                {
-                  
-                    var roleAdmin = new IdentityRole();
-                    roleAdmin.Name = "Admin";
-                    roleManager.CreateAsync(roleAdmin).Wait();
-                    userManager.AddToRoleAsync(user, roleAdmin.Name).Wait();
-                }
-            }
+    
         }
   
     }
